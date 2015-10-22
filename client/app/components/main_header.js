76c0d34a4739d364f454a933fa6fc1ee0c0ee498
Kota Ishimoto <ishikota086@gmail.com>
2015-10-22 18:54:13 +0900

    add header button

--- client/app/components/main_header.js
+++ client/app/components/main_header.js
@@ -7,6 +7,11 @@ var MainHeader = React.createClass({
         <div className="container">
           <div className="navbar-header">
             <a className="navbar-brand" href="#">Schedule Manager</a>
+          <div className="btn-toolbar pull-right">
+            <div className="main-header-btn-group btn-group">
+              <button type="button" className="btn btn-primary">Edit Schedule</button>
+            </div>
+          </div>
           </div>
         </div>
       </nav>
