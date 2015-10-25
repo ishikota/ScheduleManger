e0c6cc8dac180972409bc2c47c5568003e78dad6
Kota Ishimoto <ishikota086@gmail.com>
2015-10-25 12:30:40 +0900

    Create text resource file

--- client/app/components/welcome_hint.js
+++ client/app/components/welcome_hint.js
@@ -1,11 +1,13 @@
 var React = require('react');
+var TEXT  = require('../text_content');
 
 var WelcomeHint = React.createClass({
   render : function () {
     return (
       <div className="panel panel-default welcome-hint">
         <div className="panel-body">
-          <p>First, choose possible date for your event</p>
+          <p>{TEXT.WELCOME_FIRST_MESSAGE}</p>
+          <p>{TEXT.WELCOME_HOWTO_MESSAGE}</p>
         </div>
       </div>
       );
---
+++ client/app/text_content.js
@@ -0,0 +1,4 @@
+module.exports = {
+  WELCOME_FIRST_MESSAGE : "まずは，イベントの候補日を決めましょう．",
+  WELCOME_HOWTO_MESSAGE : "候補日を下のカレンダーでクリックしてください."
+}
